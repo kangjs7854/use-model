b3b721a0439323007dbc8671dd00286db2bd6afd
kangjs01 <kangjs01@mingyuanyun.com>
2022-09-15 14:42:34 +0800

    test: support use the old context to create model

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,46 +1,175 @@
 import { renderHook, act } from "@testing-library/react-hooks";
-import createModel  from "../src";
+import { createContext, useContext, useState } from "react";
+import createModel, { createModelByCustomHook } from "../src";
 
-const [CounterStepProvider, useCounterCtx] = createModel(
-  {
-    count: 0,
-  },
-  {
-    add(state, payload) {
-      return {
-        ...state,
-        count: state.count + payload,
-      };
+describe("test create model by state and reducer aciton", () => {
+  const [CounterStepProvider, useCounterCtx] = createModel(
+    {
+      count: 0,
     },
-    reset(state){
-      return {
-        ...state,
-        count: 0
-      };
+    {
+      add(state, payload) {
+        return {
+          ...state,
+          count: state.count + payload,
+        };
+      },
+      reset(state) {
+        return {
+          ...state,
+          count: 0,
+        };
+      },
     }
+  );
+
+  test("test incrementing", () => {
+    const { result } = renderHook(() => useCounterCtx(), {
+      wrapper: CounterStepProvider,
+    });
+
+    act(() => {
+      result.current.dispatchAction.add(1);
+    });
+
+    expect(result.current.state.count).toBe(1);
+  });
+
+  test("test reset", () => {
+    const { result } = renderHook(() => useCounterCtx(), {
+      wrapper: CounterStepProvider,
+    });
+
+    act(() => {
+      result.current.dispatchAction.reset();
+    });
+
+    expect(result.current.state.count).toBe(0);
+  });
+});
+
+describe("test create model by custom hook", () => {
+  function useCouter() {
+    const [count, setCount] = useState(0);
+
+    return {
+      state: {
+        count,
+      },
+      dispatchAction: {
+        add(payload) {
+          setCount((pre) => pre + payload);
+        },
+        reset() {
+          setCount(0);
+        },
+      },
+    };
   }
-);
 
-test("should use custom step when incrementing", () => {
-  const { result } = renderHook(() => useCounterCtx(), { wrapper :CounterStepProvider});
+  const [CounterStepProvider, useCounterCtx] =
+    createModelByCustomHook(useCouter);
 
+  test("test incrementing", () => {
+    const { result } = renderHook(() => useCounterCtx(), {
+      wrapper: CounterStepProvider,
+    });
 
-  act(() => {
-    result.current.dispatchAction.add(1)
+    act(() => {
+      result.current.dispatchAction.add(1);
+    });
+
+    expect(result.current.state.count).toBe(1);
   });
 
-  expect(result.current.state.count).toBe(1);
+  test("test reset", () => {
+    const { result } = renderHook(() => useCounterCtx(), {
+      wrapper: CounterStepProvider,
+    });
+
+    act(() => {
+      result.current.dispatchAction.reset();
+    });
+
+    expect(result.current.state.count).toBe(0);
+  });
 });
 
+describe("test create model by custom hook and exist context", () => {
+  function useCouter() {
+    const [count, setCount] = useState(0);
 
-test("should use custom step when reset", () => {
-  const { result } = renderHook(() => useCounterCtx(), { wrapper :CounterStepProvider});
+    return {
+      state: {
+        count,
+      },
+      dispatchAction: {
+        add(payload) {
+          setCount((pre) => pre + payload);
+        },
+        reset() {
+          setCount(0);
+        },
+      },
+    };
+  }
 
+  const ExistContext = createContext(null);
 
-  act(() => {
-    result.current.dispatchAction.reset()
+  function useExistContextValue() {
+    return useContext(ExistContext);
+  }
+
+  const [CounterStepProvider, useCounterCtx] = createModelByCustomHook(
+    useCouter,
+    ExistContext
+  );
+
+  test("test incrementing", () => {
+    const { result } = renderHook(() => useCounterCtx(), {
+      wrapper: CounterStepProvider,
+    });
+
+    act(() => {
+      result.current.dispatchAction.add(1);
+    });
+
+    expect(result.current.state.count).toBe(1);
   });
 
-  expect(result.current.state.count).toBe(0);
-});
+  test("test reset", () => {
+    const { result } = renderHook(() => useCounterCtx(), {
+      wrapper: CounterStepProvider,
+    });
 
+    act(() => {
+      result.current.dispatchAction.reset();
+    });
+
+    expect(result.current.state.count).toBe(0);
+  });
+
+  test("Compatibility with old ways of using context", () => {
+    const { result } = renderHook(
+      () => {
+        const ctx = useCounterCtx();
+        const existCtx = useExistContextValue();
+        return {
+          ctx,
+          existCtx,
+        };
+      },
+      {
+        wrapper: CounterStepProvider,
+      }
+    );
+
+    act(() => {
+      result.current.ctx.dispatchAction.add(1);
+    });
+
+    expect(result.current.ctx.state.count).toBe(
+      result.current.existCtx.state.count
+    );
+  });
+});
